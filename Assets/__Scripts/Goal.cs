28ea4cc3b8a6e3aa16839d902ef1ee25e7a0d787
seanstyer0 <51007216+seanstyer0@users.noreply.github.com>
2020-10-19 14:19:24 -0400

    added goal entry detection

---
+++ Assets/__Scripts/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    //a static field accessable by code anywhere
+    static public bool goalMet = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //when the trigger is hit by something
+        //check to see if its a projectile
+        if(other.gameObject.tag == "Projectile")
+        {
+            //if so, set goalMet to true
+            Goal.goalMet = true;
+            //also set the alpha of the color to the highest opacity
+            Color c =  GetComponent<Renderer>().material.color;
+            c.a = 1;
+            GetComponent<Renderer>().material.color = c;
+            print("Goal Entered");
+        }
+    }
+}
